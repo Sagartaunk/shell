a98bdca4fb980a934f98e1c1b649327ab89dbf8e
Sagar Taunk <sagartaunk@proton.me>
2026-03-26 20:12:25 +0530

    Added Pipes

--- src/executor.rs
+++ src/executor.rs
@@ -1,6 +1,7 @@
 use crate::parser;
 use nix::sys::wait::waitpid;
-use nix::unistd::{execvp, fork, ForkResult};
+use nix::unistd::{ForkResult, close, execvp, fork, pipe};
+use std::os::unix::io::{IntoRawFd, RawFd};
 pub fn exec(args: &[String]) {
     match unsafe { fork() } {
         Ok(ForkResult::Child) => {
@@ -18,5 +19,80 @@ pub fn exec(args: &[String]) {
         Err(e) => {
             eprintln!("Error : {}", e);
         }
+    }
+}
+pub fn exec_pipe(args: &Vec<Vec<String>>) {
+    let mut pipes: Vec<(RawFd, RawFd)> = Vec::new();
+    let mut pids = vec![];
+    for _i in 0..(args.len() - 1) {
+        let (read_fd, write_fd) = pipe().unwrap();
+        let read = read_fd.into_raw_fd();
+        let write = write_fd.into_raw_fd();
+        pipes.push((read, write));
+    }
+    for i in 0..(args.len()) {
+        match unsafe { fork() } {
+            Ok(ForkResult::Child) => {
+                if i == 0 {
+                    unsafe {
+                        libc::dup2(pipes[i].1, 1);
+                    };
+                    for p in &pipes {
+                        close(p.0).unwrap();
+                        close(p.1).unwrap();
+                    }
+                    let command = parser::cstring(&args[i]);
+                    let comm: Vec<&std::ffi::CStr> = command.iter().map(|c| c.as_c_str()).collect();
+                    match execvp(comm[0], &comm) {
+                        Ok(_) => continue,
+                        Err(e) => eprintln!("{}", e),
+                    }
+                    std::process::exit(1);
+                } else if i == args.len() - 1 {
+                    unsafe { libc::dup2(pipes[i - 1].0, 0) };
+                    for p in &pipes {
+                        close(p.0).unwrap();
+                        close(p.1).unwrap();
+                    }
+                    let command = parser::cstring(&args[i]);
+                    let comm: Vec<&std::ffi::CStr> = command.iter().map(|c| c.as_c_str()).collect();
+                    match execvp(comm[0], &comm) {
+                        Ok(_) => continue,
+                        Err(e) => eprintln!("{}", e),
+                    }
+                    std::process::exit(1);
+                } else {
+                    unsafe { libc::dup2(pipes[i - 1].0, 0) };
+                    unsafe {
+                        libc::dup2(pipes[i].1, 1);
+                    };
+                    for p in &pipes {
+                        close(p.0).unwrap();
+                        close(p.1).unwrap();
+                    }
+                    let command = parser::cstring(&args[i]);
+                    let comm: Vec<&std::ffi::CStr> = command.iter().map(|c| c.as_c_str()).collect();
+                    match execvp(comm[0], &comm) {
+                        Ok(_) => continue,
+                        Err(e) => eprintln!("{}", e),
+                    }
+                    std::process::exit(1);
+                }
+            }
+            Ok(ForkResult::Parent { child }) => {
+                pids.push(child);
+            }
+            Err(e) => {
+                eprintln!("{}", e);
+            }
+        }
+    }
+
+    for p in &pipes {
+        close(p.0).unwrap();
+        close(p.1).unwrap();
+    }
+    for pid in pids.iter() {
+        waitpid(*pid, None).unwrap();
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,16 @@
 use std::ffi::CString;
-pub fn parse(input: &str) -> Vec<String> {
-    input.split_whitespace().map(|s| s.to_string()).collect()
-}
 pub fn cstring(input: &[String]) -> Vec<CString> {
     input
         .iter()
         .map(|s| CString::new(s.as_str()).unwrap())
         .collect()
+}
+pub fn parse_pipeline(input: &str) -> Vec<Vec<String>> {
+    let initial: Vec<String> = input.split("|").map(|s| s.to_string()).collect();
+    let mut res: Vec<Vec<String>> = Vec::new();
+    for i in initial.iter() {
+        res.push(i.split_whitespace().map(|s| s.to_string()).collect());
+    }
+    res.retain(|r| !r.is_empty());
+    res
 }
