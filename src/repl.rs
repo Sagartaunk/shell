cf0b01793fe77a0cd1a3d6035770431cf9d32b53
Sagar Taunk <sagartaunk@proton.me>
2026-04-06 20:48:51 +0530

    Minor improvements to the job handelling

--- src/repl.rs
+++ src/repl.rs
@@ -2,17 +2,20 @@ use crate::builtins;
 use crate::executor;
 use crate::parser;
 use nix::sys::signal::{SigHandler, Signal, signal};
+use nix::sys::wait::{WaitPidFlag, WaitStatus, waitpid};
+use nix::unistd::Pid;
 use std::env;
 use std::io::{self, Write};
 
 pub fn run() {
-    let mut jobs: Vec<executor::Job> = Vec::new(); // Todo : Make this store background jobs with their state
+    let mut jobs: Vec<executor::Job> = Vec::new(); // Stores background jobs , Todo : Make it store their state
     unsafe {
         signal(Signal::SIGINT, SigHandler::SigIgn).unwrap();
         signal(Signal::SIGTSTP, SigHandler::SigIgn).unwrap();
         signal(Signal::SIGQUIT, SigHandler::SigIgn).unwrap();
     };
     loop {
+        reap_jobs(&mut jobs);
         print!("{} -> ", env::current_dir().unwrap().display());
         io::stdout().flush().unwrap();
         let mut input = String::new();
@@ -41,4 +44,36 @@ pub fn run() {
         }
         executor::exec_pipe(&input, &mut jobs);
     }
+}
+fn reap_jobs(jobs: &mut Vec<executor::Job>) {
+    jobs.retain(|job| {
+        loop {
+            match waitpid(
+                Pid::from_raw(-job.pgid.as_raw()),
+                Some(WaitPidFlag::WNOHANG),
+            ) {
+                Ok(WaitStatus::Exited(pid, status)) => {
+                    println!(
+                        "[{}] process {} exited with status {}",
+                        job.pgid, pid, status
+                    );
+                    continue;
+                }
+                Ok(WaitStatus::Signaled(pid, sig, _)) => {
+                    println!("[{}] process {} terminated by {}", job.pgid, pid, sig);
+                    continue;
+                }
+                Ok(WaitStatus::StillAlive) => return true,
+                Err(nix::errno::Errno::ECHILD) => {
+                    println!("[{}] job finished", job.pgid);
+                    return false;
+                }
+                Err(e) => {
+                    eprintln!("waitpid error for [{}]: {}", job.pgid, e);
+                    return true;
+                }
+                _ => return true,
+            }
+        }
+    });
 }
